06ba1f72424320a4fa5bbdd6fafd4849095e4a33
infosys <infosys@C4238>
2026-04-16 10:57:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample0415/Sample0415/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            //入力
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = i + 1;
+            //}
+
+            //出力
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine("array[" + i + "]:" + array[i]);
+            //}
+
+            //入力
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "] = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            //集計
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];
+            //}
+
+            Console.WriteLine();       //改行
+            //出力
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "]:");
+                astOut(array[i]);
+            }
+
+            //合計を出力
+            Console.WriteLine("合計：" + array.Where(n => n % 2 == 0).Sum());
+        }
+
+        static void astOut(int num)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();    //改行
+        }
+    }
+}
