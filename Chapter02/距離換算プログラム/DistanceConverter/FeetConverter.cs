1eaede5b13569e884ef1b3e72419cf4a963391ba
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-04-23 10:29:42 +0900

    static_readonlyに変更

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -9,16 +9,18 @@ namespace DistanceConverter
 {
     public static class FeetConverter
     {
+        public static readonly double ratio = 0.3048;    //定数
+
         // メートルからフィートを求める
         public static double FromMeter(double meter)
         {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         //フィートからメートルを求める 
         public static double ToMeter(double feet)
         {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }
