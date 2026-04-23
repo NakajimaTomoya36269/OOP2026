da42fdd91c5b703e3f2aabb23b2bf96f5251b104
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-04-23 10:39:42 +0900

    売り上げ集計プログラム作成

---
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
