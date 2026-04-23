3609eec5affe91f09ec2a8f3d32f62a365cd8152
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-04-23 09:55:11 +0900

    静的クラスにする

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -28,22 +28,20 @@
 
         static void PrintMeterToFeetList(int start, int stop)
         {
-            FeetConverter converter = new FeetConverter();
             //メートルからフィートへの対応表を出力
             for (int meter = start; meter <= stop; meter++)
             {
-                double feet = converter.FromMeter(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
         }
 
         static void PrintFeetToMeterList(int start, int stop)
         {
-            FeetConverter converter = new FeetConverter();
             //フィートからメートルへの対応表を出力
             for (int feet = start; feet <= stop; feet++)
             {
-                double meter = converter.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
         }
