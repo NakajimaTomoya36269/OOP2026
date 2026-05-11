7ccac7f0abf877626e3cb14ff19de446300b371f
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-05-11 10:45:23 +0900

    問題2.1.4完成

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // 2.1.3
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            PrintSongs(songs);
+        }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+
+            for (int i = 0; i < songs.Length; i++) {
+                int minute = songs[i].Length / 60;
+                int second = songs[i].Length % 60;
+                Console.WriteLine($"{songs[i].Title}, {songs[i].ArtistName}, {minute}:{second}");
+            }
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+
+        //2.1.2(引数付きコンストラクタ)
+        public Song(string title, string artistname, int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+
+        //2.1.1
+        //プロパティ
+        public string Title { get; set; } = string.Empty;
+
+        public string ArtistName { get; set; } = string.Empty;
+
+        public int Length { get; set; }
+    }
+}
