baea25bd9a98f34bcd9df5222ddf10b0981ad292
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-05-20 11:55:31 +0900

    問題3.1.1完成

---
+++ Chapter03/ラムダ式とLINQ/Section04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+
+namespace Section04 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+
+            var query = cities.Where(s => s.Length <= 5).ToList();
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("------");
+
+            cities[0] = "Osaka";
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise4(numbers);
+        }
+
+        private static void Exercise1(List<int> numbers) {
+            var exists = numbers.Exists(s => s % 8 == 0 || s % 9 == 0);
+            Console.WriteLine("--- 3.1.1 ---");
+            if (exists) {
+                Console.WriteLine("存在しています");
+            } else {
+                Console.WriteLine("存在しません");
+            }
+        }
+
+        private static void Exercise2(List<int> numbers) {
+
+        }
+
+        private static void Exercise3(List<int> numbers) {
+
+        }
+
+        private static void Exercise4(List<int> numbers) {
+
+        }
+    }
+}
