7424cfd29880e6b1bdbd140e9a50c52a7e89dbb8
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-05-21 10:25:31 +0900

    問題3.2.1完成

---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Console.WriteLine("***** 3.2.1 *****");
+            Exercise2_1(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.2 *****");
+            Exercise2_2(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.3 *****");
+            Exercise2_3(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.4 *****");
+            Exercise2_4(cities);
+            Console.WriteLine();
+        }
+
+        private static void Exercise2_1(List<string> cities) {
+            //できたらGitのコメント「問題3.2.1完成」
+            Console.Write("都市名：");
+            var name = Console.ReadLine();
+
+            var index = cities.FindIndex(s => s == name);
+            if(index == -1) {
+                Console.WriteLine(index);
+            } else {
+                Console.WriteLine($"{index}番目");
+            }
+        }
+
+        private static void Exercise2_2(List<string> cities) {
+	        //できたらGitのコメント「問題3.2.2完成」
+	        
+        }
+
+        private static void Exercise2_3(List<string> cities) {
+	        //できたらGitのコメント「問題3.2.3完成」
+	        
+        }
+
+        private static void Exercise2_4(List<string> cities) {
+	        //できたらGitのコメント「問題3.2.4完成」
+	        
+        }
+    }
+}
