7b552279a6a993656f860234b5ef57efa5373aec
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-05-25 11:57:32 +0900

    問題4.1.1完成

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            List<string> langs = [
+                "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+            Console.WriteLine(" --- 4.1.1 ---");
+
+            //foreach文
+            Console.WriteLine("foreach文で出力");
+
+            foreach (var lang in langs) {
+                if (lang.Contains("S")) {
+                    Console.WriteLine(lang);
+                }
+            }
+
+            //for文
+            Console.WriteLine("\nfor文で出力");
+
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains("S")) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+            //while文
+            Console.WriteLine("\nwhile文で出力");
+
+            int j = 0;
+
+            while (j < langs.Count) {
+                if (langs[j].Contains("S")) {
+                    Console.WriteLine(langs[j]);
+                }
+                j++;
+            }
+        }
+
+        private static void Exercise2(List<string> langs) {
+
+        }
+
+        private static void Exercise3(List<string> langs) {
+
+        }
+    }
+}
