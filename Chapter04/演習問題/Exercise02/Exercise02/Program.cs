d6705efbf16755dd3b3a5774eb5588c27ae57faa
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-05-27 11:12:52 +0900

    問題4.2.1完成

---
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" --- 4.2.1 ---");
+            Exercise1();
+            Console.WriteLine("\n --- 4.2.2 ---");
+            Exercise2();
+            Console.WriteLine("\n --- 4.2.3 ---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            //if-else文を使用
+            var str = Console.ReadLine();
+
+            if (int.TryParse(str, out var number)) {
+                if (number < 0) {
+                    Console.WriteLine(number);
+                } else if (number < 100) {
+                    Console.WriteLine(number * 2);
+                } else if (number < 500) {
+                    Console.WriteLine(number * 3);
+                } else {
+                    Console.WriteLine(number);
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
+        }
+
+        private static void Exercise2() {
+            //switch文を使用
+
+
+        }
+
+        private static void Exercise3() {
+            //switch式を使用
+
+
+        }
+    }
+}
