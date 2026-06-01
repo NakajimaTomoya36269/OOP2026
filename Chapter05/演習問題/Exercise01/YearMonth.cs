ad91b30af103d8448003e30e28ff943b4aaad21e
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-01 10:05:42 +0900

    問題5.1.1完成

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+
+        public string Year { get; init; }
+
+        public string Month { get; init; }
+
+        public YearMonth(string year, string month) {
+            Year = year;
+            Month = month;
+        }
+    }
+}
