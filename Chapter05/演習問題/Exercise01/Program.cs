e4a51c7435aa4f10c182db3b871425bfc3a2f20b
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-01 11:37:32 +0900

    問題5.1.2解答

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +9,15 @@ namespace Exercise01 {
     //5.1.1
     public class YearMonth {
 
-        public string Year { get; init; }
+        public int Year { get; init; }
 
-        public string Month { get; init; }
+        public int Month { get; init; }
 
-        public YearMonth(string year, string month) {
+        public YearMonth(int year, int month) {
             Year = year;
             Month = month;
         }
+
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
     }
 }
