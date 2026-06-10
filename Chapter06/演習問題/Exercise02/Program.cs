1ff22237293e3ee3a91ddd27852f141fce083383
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-10 10:27:23 +0900

    問題6.2完成

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("整数文字列：");
+            var str = Console.ReadLine();
+
+            if(int.TryParse(str, out int num)) {
+                Console.WriteLine(string.Format("{0,10:#,0}", num));
+            } else {
+                Console.WriteLine("整数文字列でありません");
+            }
+        }
+    }
+}
