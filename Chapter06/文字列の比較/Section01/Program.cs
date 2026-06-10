784fb47723409126aeea617e3756bbb653981f8b
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-10 09:56:59 +0900

    問題6.1.1完成

---
+++ Chapter06/文字列の比較/Section01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var str1 = "あいうえお";
+            var str2 = "ｱｲｳｴｵ";
+
+            var cultrueInfo = new CultureInfo("ja-JP");
+
+            if (String.Compare(str1, str2, cultrueInfo,
+                                CompareOptions.IgnoreWidth | CompareOptions.IgnoreKanaType) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
