753e2375fef7594655be9404152dc7be64c40f9d
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-10 11:32:41 +0900

    問題6.3.1完成

---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+            #region
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+            #endregion
+        }
+
+        private static void Exercise1(string text) {
+            var count = text.Count(s => s.Equals(' '));
+
+            Console.WriteLine($"空白数：{count}");
+        }
+
+        private static void Exercise2(string text) {
+            
+        }
+
+        private static void Exercise3(string text) {
+            
+        }
+
+        private static void Exercise4(string text) {
+            
+        }
+
+        private static void Exercise5(string text) {
+            
+        }
+
+        private static void Exercise6(string text) {
+            
+        }
+    }
+}
