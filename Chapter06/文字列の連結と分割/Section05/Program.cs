68a1d391756115f4662b53b0bfd358fb1ad96be4
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-15 10:36:39 +0900

    カンマ区切りで表示

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var sb = new StringBuilder();
+
+            sb.Append(String.Join(",", GetWords()));
+            Console.WriteLine(sb);
+
+            var result = String.Join(",", GetWords());
+            Console.WriteLine(result);
+        }
+
+        private static IEnumerable<string> GetWords() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
