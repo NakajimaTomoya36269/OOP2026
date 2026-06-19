4771d0f4dca8e8ba09c1f299602530b573bb0fbe
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-19 10:04:25 +0900

    問題7.1.1完成

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
+
+            var books = Books.GetBooks();
+            var priceAverage = books.Average(x => x.Price);
+            var pageAverage = books.Sum(x => x.Pages);
+            var maxPrice = books.Max(x => x.Price);
+
+            Console.WriteLine("平均金額：" + priceAverage);
+            Console.WriteLine("合計ページ：" + pageAverage);
+            Console.WriteLine("高価な本：" + maxPrice);
+
+            Console.WriteLine("--- 500円以上のタイトル ---");
+            foreach (var book in books.Where(x => x.Price >= 500)) {
+                Console.WriteLine(book.Title);
+            }
+
+            Console.WriteLine();
+            //２５０ページ以上の本を上位3冊出力
+            foreach (var results in books.Where(x => x.Pages >= 250).Take(3)) {
+                Console.WriteLine(results.Title);
+            }
+        }
+    }
+}
---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+            #region
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("\n7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("\n7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("\n7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("\n7.1.5");
+            Exercise5(numbers);
+            #endregion
+        }
+
+        private static void Exercise1(int[] numbers) {
+            var max_number = numbers.Max();
+            Console.WriteLine(max_number);
+        }
+
+        private static void Exercise2(int[] numbers) {
+
+        }
+
+        private static void Exercise3(int[] numbers) {
+            
+        }
+
+        private static void Exercise4(int[] numbers) {
+            
+        }
+
+        private static void Exercise5(int[] numbers) {
+            
+        }
+    }
+}
