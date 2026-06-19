0a7e77d0005d67c881e45cff2507e4b08edc533a
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-19 10:05:23 +0900

    問題7.1.2完成

--- Chapter07/演習問題/Exercise01/Program.cs
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -27,7 +27,9 @@ namespace Exercise01 {
         }
 
         private static void Exercise2(int[] numbers) {
-
+            foreach (var item in numbers.TakeLast(2)) {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise3(int[] numbers) {
