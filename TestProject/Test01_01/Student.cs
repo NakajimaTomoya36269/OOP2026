fb38a9bc549440751b1b6b1ce8705a735e844efe
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-22 11:26:32 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,11 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
 
+        public required string Name { get; init; }
+
+        public required string Subject { get; init; }
+
+        public required int Score { get; init; }
 
     }
 }
