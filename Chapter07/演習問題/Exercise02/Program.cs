91b87735fa4bd7790c605f2dab479a918275702e
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-22 09:48:26 +0900

    問題7.2.7完成

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -68,7 +68,10 @@ namespace Exercise02 {
         }
 
         private static void Exercise7(List<Book> books) {
-            
+            var selected = books.Where(b => b.Title.Contains("C#") && b.Pages <= 500).Select(b => b.Title);
+            foreach (var item in selected) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
