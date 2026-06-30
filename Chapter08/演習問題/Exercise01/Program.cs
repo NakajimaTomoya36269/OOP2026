e875257b3182a6175a78a1a9e40b534dab20ad0b
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-06-30 14:06:47 +0900

    問題8.1.1完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -4,17 +4,35 @@ namespace Exercise01 {
         static void Main(string[] args) {
             var text = "Cozy lummox gives smart squid who asks for job pen";
 
+            Console.WriteLine("問題8.1.1");
             Exercise1(text);
             Console.WriteLine();
+            Console.WriteLine("問題8.1.2");
             Exercise2(text);
         }
 
         private static void Exercise1(string text) {
-            
+            //コミットのコメント(問題8.1.1完成)
+            var str = text.ToUpper();
+            var dict = new Dictionary<char, int>();
+
+            foreach (var ch in str) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        ++dict[ch];
+                    } else {
+                        dict[ch] = 1;
+                    }
+                }
+            }
+            foreach (var sortDict in dict.OrderBy(d => d.Key)) {
+                Console.WriteLine($"'{sortDict.Key}':{sortDict.Value}");
+            }
         }
 
         private static void Exercise2(string text) {
-            
+            //コミットのコメント(問題8.1.2完成)
+
         }
     }
 }
