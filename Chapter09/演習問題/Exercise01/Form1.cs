10fefc4aa26a1efdc98ecb748543f7d9574307a6
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-07-09 14:28:17 +0900

    問題9.1完成

--- Chapter09/演習問題/Exercise01/Form1.cs
+++ Chapter09/演習問題/Exercise01/Form1.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace Exercise01 {
     public partial class Form1 : Form {
         public Form1() {
@@ -6,14 +8,19 @@ namespace Exercise01 {
 
         private void btButton1_Click(object sender, EventArgs e) {
             var dateTime = DateTime.Now;
+            tbOut1.Text = dateTime.ToString();
         }
 
         private void btButton2_Click(object sender, EventArgs e) {
             var dateTime = DateTime.Now;
+            tbOut2.Text = dateTime.ToString("yyyy”NMMŒŽdd“ú HHŽžmm•ªss•b");
         }
 
         private void btButton3_Click(object sender, EventArgs e) {
             var dateTime = DateTime.Now;
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            tbOut3.Text = dateTime.ToString("gg y”N MŒŽ d“ú(dddd)", culture);
         }
     }
 }
