7e5b9d8aaa5164d4b07f3d551acd941913ff8f8f
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-07-09 15:48:44 +0900

    Stopwatchクラス使用

--- Chapter09/演習問題/Exercise03/Form1.Designer.cs
+++ Chapter09/演習問題/Exercise03/Form1.Designer.cs
@@ -23,9 +23,14 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            components = new System.ComponentModel.Container();
             btStart = new Button();
             btStop = new Button();
             lbTimeDisp = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            btReset = new Button();
+            listBox1 = new ListBox();
+            button1 = new Button();
             SuspendLayout();
             // 
             // btStart
@@ -57,12 +62,49 @@
             lbTimeDisp.Size = new Size(343, 40);
             lbTimeDisp.TabIndex = 1;
             // 
+            // timer1
+            // 
+            timer1.Interval = 1;
+            timer1.Tick += timer1_Tick;
+            // 
+            // btReset
+            // 
+            btReset.Location = new Point(37, 154);
+            btReset.Name = "btReset";
+            btReset.Size = new Size(163, 47);
+            btReset.TabIndex = 2;
+            btReset.Text = "リセット";
+            btReset.UseVisualStyleBackColor = true;
+            btReset.Click += btReset_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(413, 31);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(173, 184);
+            listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(217, 154);
+            button1.Name = "button1";
+            button1.Size = new Size(163, 47);
+            button1.TabIndex = 4;
+            button1.Text = "ラップタイム";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(192, 255, 255);
-            ClientSize = new Size(437, 163);
+            ClientSize = new Size(598, 230);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(btReset);
             Controls.Add(lbTimeDisp);
             Controls.Add(btStop);
             Controls.Add(btStart);
@@ -76,5 +118,9 @@
         private Button btStart;
         private Button btStop;
         private Label lbTimeDisp;
+        private System.Windows.Forms.Timer timer1;
+        private Button btReset;
+        private ListBox listBox1;
+        private Button button1;
     }
 }
--- Chapter09/演習問題/Exercise03/Form1.cs
+++ Chapter09/演習問題/Exercise03/Form1.cs
@@ -1,33 +1,48 @@
+using System.Diagnostics;
+
 namespace Exercise03 {
     public partial class Form1 : Form {
-        TimeWatch tw;
+        Stopwatch sw = new Stopwatch();
 
         public Form1() {
             InitializeComponent();
         }
 
         private void btStart_Click(object sender, EventArgs e) {
-            tw = new TimeWatch();
-            tw.Start();
-            lbTimeDisp.Text = "Œv‘ª’†";
+            sw.Start();
+            timer1.Start();
         }
 
         private void btStop_Click(object sender, EventArgs e) {
-            tw.Stop();
-            TimeSpan duration = tw.Stop();
-            lbTimeDisp.Text = $"{duration.TotalSeconds}•b";
+            sw.Stop();
+            timer1.Stop();
+            lbTimeDisp.Text = $"{sw.Elapsed}•b";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            lbTimeDisp.Text = $"{sw.Elapsed}•b";
+        }
+
+        private void btReset_Click(object sender, EventArgs e) {
+            sw.Reset();
+            lbTimeDisp.Text = $"{sw.Elapsed}•b";
+        }
+
+        //ƒ‰ƒbƒvƒ^ƒCƒ€
+        private void button1_Click(object sender, EventArgs e) {
+            listBox1.Items.Insert(0, lbTimeDisp.Text);
         }
     }
 }
 
-internal class TimeWatch {
-    private DateTime _time;
+//internal class TimeWatch {
+//    private DateTime _time;
 
-    public void Start() {
-        _time = DateTime.Now;
-    }
+//    public void Start() {
+//        _time = DateTime.Now;
+//    }
 
-    public TimeSpan Stop() {
-        return DateTime.Now - _time;
-    }
-}
+//    public TimeSpan Stop() {
+//        return DateTime.Now - _time;
+//    }
+//}
