31f4860d6d0867eb536c0a4f2e762ec2ce82d4ef
中嶋　智也 <oibc36269@ojs.ac.jp>
2026-09-01 15:49:45 +0900

    SQLite商品管理アプリ完成

--- FormApps/SQLiteProductSample/ProductRepository.cs
+++ FormApps/SQLiteProductSample/ProductRepository.cs
@@ -1,4 +1,6 @@
 using Microsoft.Data.Sqlite;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace SQLiteProductSample;
 
@@ -35,6 +37,74 @@ public class ProductRepository
             });
         }
         return products;
+
+    }
+
+    //商品を1件追加する。Create(INSERT)に担当する
+    //戻り値として自動採番されたIdを返す
+    public int Add(string name, int price) {
+        //接続オブジェクトを生成する
+        using var connection = Database.GetConnection();
+        connection.Open();
+        using var command = connection.CreateCommand();
+
+        command.CommandText =
+            """
+            INSERT INTO Products (Name, Price)
+            VALUES ($name, $price);
+
+            SELECT last_insert_rowid();
+            """;
+
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$price", price);
+
+        //一つの値を返すSQLを実行する
+        var result = command.ExecuteScalar();
+
+        if (result is null)
+            throw new InvalidOperationException("登録した商品のIDを取得できませんでした。");
+
+        return Convert.ToInt32((long)result);
+    }
+
+    public void Update(Product product) {
+        //接続オブジェクトを生成する
+        using var connection = Database.GetConnection();
+        connection.Open();
+        using var command = connection.CreateCommand();
+
+        command.CommandText =
+            """
+            UPDATE Products
+            SET Name = $name,
+                Price = $price
+            WHERE Id = $id;
+            """;
+
+        command.Parameters.AddWithValue("$name", product.Name);
+        command.Parameters.AddWithValue("$price", product.Price);
+        command.Parameters.AddWithValue("$id", product.Id);
+
+        if (command.ExecuteNonQuery() == 0)
+            throw new InvalidOperationException("修正対象の商品が見つかりませんでした。");
+    }
+
+    public void Delete(int id) {
+        using var connection = Database.GetConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText =
+            """
+            DELETE FROM Products
+            WHERE Id = $id;
+            """;
+
+        command.Parameters.AddWithValue("$id", id);
+
+        if (command.ExecuteNonQuery() == 0)
+            throw new InvalidOperationException("修正対象の商品が見つかりませんでした。");
 
     }
 }
